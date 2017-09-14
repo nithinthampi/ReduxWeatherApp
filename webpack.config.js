ab04fa2ecb1a89ac416fa4bcfb62e9ce8462fc5e
nithin <nithin.thampi87@gmail.com>
2017-09-14 17:54:46 +0530

    Upgraded react  to new version

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,38 @@
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
 module.exports = {
   entry: [
-    './src/index.js'
+    './src/index.js',
   ],
   output: {
     path: __dirname,
     publicPath: '/',
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   module: {
     loaders: [{
-      exclude: /node_modules/,
-      loader: 'babel',
+      test: /\.jsx?$/,
+      exclude: /(node_modules|bower_components)/,
+      loader: 'babel-loader',
       query: {
-        presets: ['react', 'es2015', 'stage-1']
-      }
-    }]
+        presets: ['react', 'es2015', 'stage-0'],
+      },
+    },
+    {
+      test: /\.css$/,
+      loaders: ['style-loader', 'css-loader', 'autoprefixer-loader?browsers=last 2 versions'],
+    },
+    {
+      test: /\.scss$/,
+      loaders: ['style-loader', 'css-loader', 'autoprefixer-loader?browsers=last 2 versions', 'sass-loader'],
+    }],
   },
   resolve: {
-    extensions: ['', '.js', '.jsx']
+    extensions: ['.js', '.jsx'],
   },
   devServer: {
     historyApiFallback: true,
     contentBase: './',
-    disableHostCheck: true,
-    host:"0.0.0.0",
-    port:8080
-  }
+  },
+  devtool: process.env.NODE_ENV === 'production' ? undefined : 'cheap-module-eval-source-map',
 };
